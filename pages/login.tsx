d4de5d55ad2e4007cc27913a65c1bbf029a23db8
kentoda <kent19950811@gmail.com>
2021-01-30 10:46:43 +0900

    Create login reacthooks

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,27 +1,27 @@
 import Link from "next/link";
 
-const Signup: React.FC = () => {
+const Login: React.FC = () => {
   return (
-    <div className="min-h-screen flex bg-gray-200">
+    <div className="min-h-screen flex flex-col justify-center bg-gray-200">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="text-center mt-24">
-          <h2 className="mt-6 text-center text-3xl leading-9 font-   extrabold text-gray-900">
-            SignUp
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Login
           </h2>
           <p className="mt-2 text-center text-md text-gray-600">
-            {"既にアカウントをお持ちですか?"}
-            <Link href="/login">
+            {"アカウントをお持ちではないですか？"}
+            <Link href="/signup">
               <a href="#" className="text-blue-500">
-                LogIn
+                Signup
               </a>
             </Link>
           </p>
         </div>
         <div className="mt-8 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          {/* SignUpFormコンポーネントをここ */}
+          {/* ここにフォーム */}
         </div>
       </div>
     </div>
   );
 };
-export default Signup;
+export default Login;
