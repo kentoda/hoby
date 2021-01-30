c23866649e0cff5c0b78ec7db9b383e83f9b0728
kentoda <kent19950811@gmail.com>
2021-01-30 10:45:19 +0900

    Link for

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,9 +9,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Link href="/signup">
+        <a>SignUpページ</a>
+      </Link>
+
       <div className="mt-4 p-4 w-1/4 rounded bg-red-800 text-center">
         <p className="text-blue-300">この要素は赤です</p>
       </div>
     </div>
-  )
+  );
 }
